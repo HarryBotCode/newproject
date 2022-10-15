aded2e5a738fdb580cf391e61bf856e42df2ab27
harris595 <harisjavedawan123@gmail.com>
2022-10-15 18:24:32 +0500

    firstcom

---
+++ react-practice/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import NavBar from './NavBar';
+import Body from './Body';
+import Footerr from './Footerr';
+
+
+
+
+const App = () => {
+  
+  return (
+    <div>
+      <NavBar /> 
+      <Body />
+      <Footerr />
+      </div>
+  )
+}
+
+export default App;
+
